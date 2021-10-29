a332a59c5f7f8c3a1550e52024e4c9550a278f20
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-10-29 18:05:16 +0530

    Register User Function was added with bcrypt...

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,7 +1,36 @@
 const router = require("express").Router();
+const User = require("../Models/UserModel");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send(`Hello Auth from router!!`);
+//REGISTER
+router.get("/", async (req, res) => {
+  res.send(`Hello register from router!!`);
 });
+
+router.post("/register", async (req, res) => {
+  try {
+    //Generate Salt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Created the user by taking input from user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //Save user and return response
+    const user = await newUser.save();
+    console.log(user);
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("not ok");
+  }
+});
+
+//lOGIN
+router;
 
 module.exports = router;
