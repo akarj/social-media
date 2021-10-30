b422bf60b0b5b6e7d75da6f7826c43c7877a5d86
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-10-30 12:57:30 +0530

    Following the user request was completed!!

--- Routes/UsersRoute.js
+++ Routes/UsersRoute.js
@@ -70,6 +70,32 @@ router.get("/:id", async (req, res) => {
 });
 
 //[Follow User]
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        return res.status(200).json({
+          message: "Requested User has been followed by the current User!!",
+        });
+      } else {
+        return res
+          .status(403)
+          .json({ message: "User Already follows the requested user" });
+      }
+    } catch (err) {
+      console.log("Error in following the User");
+      res.status(500).json({ message: "Error in following the User", err });
+    }
+  } else {
+    res.status(403).json({
+      message: "You can't follow yourself!!",
+    });
+  }
+});
 
 //[Unfollow User]
 router.get("/", (req, res) => {
