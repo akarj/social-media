2fbed03d65721dbaa1bf1d95c7d965d99a9dcaff
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-11-03 13:00:38 +0530

    Rightbar UI finished!!

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -8,6 +8,10 @@ export default function Feed() {
       <div className="feedContainer">
         <Share />
         <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,139 @@
 import "./Rightbar.scss";
+import { FaBirthdayCake } from "react-icons/fa";
 
 export default function Rightbar() {
   return (
     <div className="rightbarContainer">
-      Rightbar
-      <span>rightbar span</span>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <FaBirthdayCake className="birthdayIcon" />
+          <span className="birthdayText">
+            <b>John Wick </b>and <b>3 other friends </b>have birthday today
+          </span>
+        </div>
+        <div className="advertisementImgDiv">
+          <img
+            src="https://source.unsplash.com/weekly?water"
+            alt="advertisement"
+            className="rightbarAd"
+          />
+        </div>
+        <h4 className="rightbarBottomTitle">Online Friends</h4>
+        <section className="rightbarActiveFriendListContainerSection">
+          <ul className="rightbarBottomFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarBottomProfileImgContainer">
+                <img
+                  src="https://source.unsplash.com/weekly?person"
+                  alt="friend"
+                  className="rightbarProfileImg"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">John Doe</span>
+            </li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
