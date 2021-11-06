11fe6c1f4cf52c8609f4603fedf2d29977f6dec2
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-11-06 13:12:06 +0530

    React Router 6 was implemented!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login/Login";
 import RegisterPage from "./pages/Register/RegisterPage";
-// import Login from "./pages/Login/Login";
-// import RegisterPage from "./pages/Register/RegisterPage";
-// import ProfilePage from "./pages/profile/ProfilePage";
-// import Homepage from "./pages/Homepage/Homepage";
-// import { FcLike } from "react-icons/fc";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ProfilePage from "./pages/profile/ProfilePage";
+import Homepage from "./pages/Homepage/Homepage";
+
 export default function App() {
   return (
-    <div>
-      {/* <Homepage /> */}
-      {/* <Login /> */}
-      <RegisterPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="profile/*" element={<ProfilePage />} />
+      </Routes>
+    </Router>
   );
 }
