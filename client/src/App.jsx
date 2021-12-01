6989b6e7b88ef627507bc42a24209402f63f654e
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-12-01 23:07:27 +0530

    Navigate from router was added with user condition!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Login from "./pages/Login/Login";
 import RegisterPage from "./pages/Register/RegisterPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import Homepage from "./pages/Homepage/Homepage";
+import { AuthContext } from "./context/AuthContext";
 
 export default function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={user ? <Homepage /> : <RegisterPage />}
+        ></Route>
+        <Route path="login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="register"
+          element={user ? <Navigate to="/" /> : <RegisterPage />}
+        />
         <Route path="profile/:username" element={<ProfilePage />} />
       </Routes>
     </Router>
